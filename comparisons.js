fa3f35ee2985cb188a2f83a4f549234c6d0896b2
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-07 14:14:19 -0700

    Add my answers to comparisons.js

--- comparisons.js
+++ comparisons.js
@@ -17,12 +17,15 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log(numberTeachers === numberStudents);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log(numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log(numberTeachers >= numberStudents);
 // this should log: false
 
 
@@ -33,14 +36,18 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // For the following prompts, you will be given a line of code and your task is to type out a comment,in English, explaining what that line of code is doing, including what the comparison will evaluate to.
 
 var friends = 6;
-var siblings = 2;
-console.log(friends > siblings);
-// YOU DO: Explain.
+var siblings = 2; 
+console.log(friends > siblings); 
+// YOU DO: Explain. 
+//This will output "true" because the variable "friends" is equal to 6 while the variable "siblings" is equal to 2. 
+// In my own words this line is asking: "Is 6 greater than 2?" 6 is greater than 2, the terminal must output "true".
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
+//This will output "true" because the variable attendees is equal to 9 while the variable "meals" is equal to 8.
+//In my own words this line is asking: "Is 9 not equal to 8?" Since 9 does not equal 8, the terminal must output "true".
 
 
 // #-------------------
@@ -63,10 +70,13 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log(lovesToPlay && lovesTreats);
 
 // YOU DO:
 // Determine if the dog loves to play or loves the dog park
+console.log(lovesToPlay || lovesDogPark);
 
 // YOU DO:
 // Determine if the dog loves to play and is a puppy
 // HINT: Use the age variable and assume that a puppy is less than 2 years old
+console.log(lovesToPlay && (age < 2));
